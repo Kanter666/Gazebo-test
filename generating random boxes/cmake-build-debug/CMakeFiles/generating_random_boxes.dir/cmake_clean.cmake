a14556cb8cbc9774bc5551360b62545bc05c7cb9
Adam <ak2g15@soton.ac.uk>
2017-07-13 02:13:14 +0100

    Setting up CLion and boost libraries

---
+++ generating random boxes/cmake-build-debug/CMakeFiles/generating_random_boxes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generating_random_boxes.dir/main.cpp.o"
+  "generating_random_boxes.pdb"
+  "generating_random_boxes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/generating_random_boxes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
