7f7239ed71a029ecdb03b658e2d683b7b26eb7d3
Adam <ak2g15@soton.ac.uk>
2017-07-13 02:24:27 +0100

    Creating new folders

--- generating random boxes/main.cpp
+++ generating random boxes/main.cpp
@@ -3,6 +3,13 @@
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string folder = "Hello";
+
+    namespace fs = boost::filesystem;
+    boost::filesystem::path full_path = fs::system_complete("../Models/"+folder);
+
+    if(boost::filesystem::create_directory(full_path)) {
+        std::cout << "Success" << "\n";
+    }
     return 0;
 }
