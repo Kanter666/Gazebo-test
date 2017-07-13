4619134154285088e1ef5d0c66240a05fa0d201c
Adam <ak2g15@soton.ac.uk>
2017-07-13 13:32:37 +0100

    File folder fix

--- generating random boxes/main.cpp
+++ generating random boxes/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <boost/filesystem.hpp>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 
 int main() {
-    std::string folder = "Hello";
+    string folder = "Hello";
 
     namespace fs = boost::filesystem;
     boost::filesystem::path full_path = fs::system_complete("../Models/"+folder);
 
     if(boost::filesystem::create_directory(full_path)) {
-        std::cout << "Success" << "\n";
-        std::ofstream file(full_path + "/model.config"); //open in constructor
-        std::string data("inside config");
-        file << data;
+        cout << "Success" << "\n";
     }
+
+    ofstream myfile;
+    cout << ("../Models/"+folder+"/model.config");
+    myfile.open ("../Models/"+folder+"/model.config");
+    myfile << "Here will be config.\n";
+    myfile.close();
+
     return 0;
+}
+
+void create_sdf(double width, double length, double depth, string file_name){
+    cout << "Inside create sdf";
 }
